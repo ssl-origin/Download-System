bf7c4f8128bfebca39490180ce89e12e8622eeb6
Fred <62480197+ssl-origin@users.noreply.github.com>
2021-08-08 22:53:57 +0200

    update core

--- language/fr/permissions_downloadsystem.php
+++ language/fr/permissions_downloadsystem.php
@@ -1,1 +1,43 @@
+<?php
+/**
+*
+* Download System [French]
+* 
+* @package phpBB Extension - Download System
+* @copyright (c) 2016 dmzx - https://www.dmzx-web.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters for use
+// ’ » “ ” …
+
+$lang = array_merge($lang, array(
+	'ACL_U_DM_EDS_DOWNLOAD'				=> 'Peut télécharger des fichiers dans le système de téléchargement',
+	'ACL_U_DM_EDS_UPLOAD'				=> 'Peut envoyer des fichiers dans le système de téléchargement',
+	'ACL_U_DM_EDS_USE'					=> 'Peut utiliser le système de téléchargement',
+	'ACL_U_DM'							=> 'Système de téléchargement',
+));
 
